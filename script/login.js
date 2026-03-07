6819719977b2caafd7a8ee79c01ca311ab69dd2c
mahmudulhasanzb <mahmudulhasankk9@gmail.com>
2026-03-08 01:08:20 +0600

    navbar design done

--- script/login.js
+++ script/login.js
@@ -1,1 +1,23 @@
-console.log("Login page script loaded");
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const signInBtn = document.getElementById('sign-in-btn');
+
+// sign in function
+const signIn = () => {
+  // get the value of username and password
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  // check if the username and password is correct
+  if (username !== "admin") {
+    alert("Invalid username");
+    return;
+  }
+  if (password !== "admin123") {
+    alert("Invalid password");
+    return;
+  }
+  if (username === "admin" && password === "admin123") {
+    window.location.replace("index.html");
+  }
+}
